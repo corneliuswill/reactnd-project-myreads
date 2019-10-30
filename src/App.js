6bbb3691c2a2d92fd3062b756c7b35a1863024bb
Cornelius Williams <corneliusw@outlook.com>
2019-10-29 23:20:03 -0500

    refactor: use Routes to control page content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <Link className="close-search"
+                to='/'
+              ></Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -38,7 +34,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -199,11 +196,11 @@ class BooksApp extends React.Component {
                 to="/search"
               ></Link>
             </div>
-          </div>
-        )}
+          </div>     
+        )} />
       </div>
     )
   }
-}
+}        
 
 export default BooksApp
